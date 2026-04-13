ecfa7b341cf078346390490fd7061d81c0c50781
israelmelo <israelg.8@gmail.com>
2026-04-13 07:59:54 -0300

    Remove endpoint do basev1 evaluationmanager e arquivos relacionados

--- src/modules/BaseV1EmbedTools/Controller.php
+++ src/modules/BaseV1EmbedTools/Controller.php
@@ -29,15 +29,6 @@ class Controller extends \MapasCulturais\Controllers\Opportunity
         $this->render("form-builder", ['entity' => $entity]);
     }
 
-    public function GET_evaluationmanager()
-    {
-        $entity = $this->getEntityAndCheckPermission('@control');
-
-        $evaluation_method = $entity->getEvaluationMethod();
-
-        $this->render("evaluation-manager", ['entity' => $entity, 'evaluation_method' => $evaluation_method]);
-    }
-
     public function GET_opportunityresults()
     {
         $app = App::i();
