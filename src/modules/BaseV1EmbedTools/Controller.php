08ff0d60fbd2e51a8188f6a810ac35081bdb07bc
israelmelo <israelg.8@gmail.com>
2026-04-13 08:17:45 -0300

    Remove endpoint do basev1 registrationevaluationtionformview

--- src/modules/BaseV1EmbedTools/Controller.php
+++ src/modules/BaseV1EmbedTools/Controller.php
@@ -48,14 +48,6 @@ class Controller extends \MapasCulturais\Controllers\Opportunity
         $this->render("report-manager",['entity' => $entity]);
     }
 
-     public Function GET_registrationevaluationtionformview()
-    {
-        $this->entityClassName = "MapasCulturais\\Entities\\Registration";
-        $this->layout = "embedtools-registration";
-        $entity = $this->getEntityAndCheckPermission('view');
-        $this->render("registration-view",['entity' => $entity, 'merge_phases' => true]);
-    }
-
     public Function GET_fieldsvisible()
     {
         $entity = $this->getEntityAndCheckPermission('@control');
